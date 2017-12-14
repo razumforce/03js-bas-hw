2e44ad702146eb164c389afec4979f14e0efadbc
Razum Force <razum@Razums-iMac.local>
2017-12-14 23:39:58 +0300

    first version of gallery working

--- 06HW/js/task06_1.js
+++ 06HW/js/task06_1.js
@@ -4,7 +4,24 @@
 window.onload = init;
 
 function init() {
-  console.log('инициализация скрипта');
+  var images = document.getElementsByTagName('img');
+  for (var i = 0; i < images.length; i++) {
+    images[i].onclick = changeBigPicture;
+  }
+  var img1 = document.getElementById('image1');
+  var event = new Event('click');
+  img1.dispatchEvent(event);
+}
+
+function changeBigPicture(eventObj) {
+  var appDiv = document.getElementById('big_picture');
+  appDiv.innerHTML = '';
+  var eventElement = eventObj.target;
+  var imageNamePart = eventElement.src.split('/').reverse()[0].replace('_s', '');
+  var src = 'img/orig/' + imageNamePart;
+  var imageDomElement = document.createElement('img');
+  imageDomElement.src = src;
+  appDiv.appendChild(imageDomElement);
 }
 
 function main() {
